dc17de1465efd40f1a1267ca59b8e02caaddd4b3
Clay Jones <cgjones0711@gmail.com>
2021-04-07 22:37:39 -0500

    pushing for help

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,15 @@
 $url1 = '<a href="[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]"</a>';
 $url2 = '<a href="[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]"</a>';
 $url3 = '<a href="[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]"</a>';
-$badgeAnswer = $answers.contact};
-function chooseBadge(){
-  if ( $badgeAnswer = true)
-    { return ("url1")}
-
-)
+$badgeAnswer = ${answers.contact};
+function generateMarkdown(){
+  if ( $badgeAnswer = "apache") 
+  { return ("$url1")}
+ else if ($badgeAnswer = "boost") {
+    return("$url2")  
+ } else { return ("$url3")
+   
+ }
 function generateMarkdown(data) {
   return `# ${data.title}
 
